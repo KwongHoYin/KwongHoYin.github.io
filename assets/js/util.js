1af4a3b5146556389792059325a2a2f07efbfccd
KwongHoYin <95200927+KwongHoYin@users.noreply.github.com>
2023-03-03 17:41:50 +0800

    Add files via upload

--- assets/js/util.js
+++ assets/js/util.js
@@ -584,4 +584,22 @@
 
 	};
 
+
+//Back to top button	
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
 })(jQuery);
